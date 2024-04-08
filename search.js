e0b5ce79e1c2e89a1165dac711c3714bacbcae8c
0xA9F9 <62254177+0xA9F9@users.noreply.github.com>
2024-04-09 00:22:10 +0200

    Create search.js

---
+++ search.js
@@ -0,0 +1,59 @@
+document.addEventListener("DOMContentLoaded", function() {
+    var inputElement = document.querySelector(".i-s input");
+    var parentElement = inputElement.parentNode;
+
+    inputElement.addEventListener("click", function(event) {
+
+        if (!parentElement.classList.contains("active")) {
+            parentElement.classList.add("active");
+        }
+        event.stopPropagation(); 
+    });
+
+    document.addEventListener("click", function(event) {
+
+        if (!parentElement.contains(event.target)) {
+
+            parentElement.classList.remove("active");
+        }
+    });
+});
+
+
+function toggleClearButtonVisibility() {
+    const searchInput = document.getElementById('search');
+    const clearButton = document.getElementById('clear-button');
+
+    if (searchInput.value.trim() !== '') {
+        clearButton.style.display = 'inline-block';
+    } else {
+        clearButton.style.display = 'none';
+    }
+}
+
+function clearSearchAndSubmit() {
+    const searchInput = document.getElementById('search');
+    searchInput.value = ''; 
+    searchTerm = ''; 
+
+    const event = new Event('input', {
+        bubbles: true,
+        cancelable: true,
+    });
+    searchInput.dispatchEvent(event);
+}
+
+const clearButton = document.createElement('button');
+clearButton.id = 'clear-button';
+clearButton.className = 'clear-button';
+clearButton.textContent = '×';
+clearButton.style.display = 'none'; // Изначально скрываем кнопку
+
+clearButton.addEventListener('click', clearSearchAndSubmit);
+
+const searchInput = document.getElementById('search');
+searchInput.parentNode.insertBefore(clearButton, searchInput.nextSibling);
+
+searchInput.addEventListener('input', toggleClearButtonVisibility);
+
+toggleClearButtonVisibility();
