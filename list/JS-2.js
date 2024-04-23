e34376d40be19e8916071c4f11d69c68a52212f1
0xA9F9 <62254177+0xA9F9@users.noreply.github.com>
2024-04-23 21:05:25 +0200

    Update JS-2.js

--- list/JS-2.js
+++ list/JS-2.js
@@ -1,21 +1,25 @@
-      function changeLanguage(lang) {
+
+document.addEventListener("DOMContentLoaded", function () {
+    var menu = `
+        <div id="click-menu" class="click-menu" style="position: fixed; padding: 10px; border-radius: 5px; display: none;"></div>
+    `;
+    document.body.insertAdjacentHTML("beforeend", menu);
+
+    document.addEventListener("click", function (e) {
         var clickMenu = document.getElementById("click-menu");
-        var ruButton = document.getElementById("ruButton");
-        var enButton = document.getElementById("enButton");
-
-        if (lang === 'ru') {
-            clickMenu.textContent = "не трогай код!";
-            ruButton.classList.add("active");
-            enButton.classList.remove("active");
-        } else if (lang === 'en') {
-            clickMenu.textContent = "don't touch the code!";
-            enButton.classList.add("active");
-            ruButton.classList.remove("active");
+        if (e.button !== 2 && window.getComputedStyle(clickMenu).display === "block") {
+            clickMenu.style.display = "none";
         }
-    }
+    });
 
-document.addEventListener("DOMContentLoaded",function(){var e=`
-        <div id="click-menu" class="click-menu" style="padding: 10px; border-radius: 5px;"></div>
-    `;document.body.insertAdjacentHTML("beforeend",e),document.addEventListener("click",function(e){var t=document.getElementById("click-menu");2!==e.button&&"block"===t.style.display&&(t.style.display="none")}),document.documentElement.addEventListener("contextmenu",function e(t){t.preventDefault();var n=document.getElementById("click-menu");n.style.display="block",n.style.left=t.clientX+"px",n.style.top=t.clientY+"px"})});
+    document.documentElement.addEventListener("contextmenu", function (e) {
+        e.preventDefault();
+        var clickMenu = document.getElementById("click-menu");
+        clickMenu.style.display = "block";
+        clickMenu.style.left = e.clientX + "px";
+        clickMenu.style.top = e.clientY + "px";
+    });
+});
+
 
 document.addEventListener("DOMContentLoaded",function(){var n=document.getElementById("searchInput");n.addEventListener("focus",function(){this.classList.add("on")}),n.addEventListener("blur",function(){this.classList.remove("on")})});
