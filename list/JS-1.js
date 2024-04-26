4533f36722ce0a2dfcdf773125ea503d06f318bc
0xA9F9 <62254177+0xA9F9@users.noreply.github.com>
2024-04-26 03:25:10 +0200

    Update JS-1.js

--- list/JS-1.js
+++ list/JS-1.js
@@ -59,34 +59,3 @@ window.onscroll = function() {
 function scrollToTop() {
   document.body.scrollTop = 0;
   document.documentElement.scrollTop = 0; 
-
-
-
-//ripple
-
-document.addEventListener('DOMContentLoaded', function() {
-      const buttons = document.querySelectorAll('button');
-      buttons.forEach(button => {
-        button.addEventListener('click', createRipple);
-      });
-    });
-
-    function createRipple(event) {
-      const button = event.currentTarget;
-      const circle = document.createElement("span");
-      const diameter = Math.max(button.clientWidth, button.clientHeight);
-      const radius = diameter / 2;
-
-      circle.style.width = circle.style.height = `${diameter}px`;
-      circle.style.left = `${event.clientX - button.offsetLeft - radius}px`;
-      circle.style.top = `${event.clientY - button.offsetTop - radius}px`;
-      circle.classList.add("ripple");
-
-      const ripple = button.getElementsByClassName("ripple")[0];
-
-      if (ripple) {
-        ripple.remove();
-      }
-
-      button.appendChild(circle);
-    }
