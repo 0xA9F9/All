ef7fbf38b7601b7e6f152aa4ca5148b78cfa4aa1
0xA9F9 <62254177+0xA9F9@users.noreply.github.com>
2024-05-05 23:44:02 +0200

    Update JS.js

--- list/JS.js
+++ list/JS.js
@@ -21,16 +21,16 @@ function changeLanguage(lang) {
     var enButton = document.getElementById("enButton");
     var footerText = document.getElementById("footerText");
 
-    if (lang === 'en') {
-        clickMenu.textContent = "don't touch the code!";
-        footerText.textContent = "© 2024 All rights reserved.";
-        enButton.classList.add("active");
-        ruButton.classList.remove("active");
-    } else if (lang === 'ru') {
+    if (lang === 'ru') {
         clickMenu.textContent = "не трогай код!";
         footerText.textContent = "© 2024 Все права защищены.";
         ruButton.classList.add("active");
         enButton.classList.remove("active");
+    } else if (lang === 'en') {
+      clickMenu.textContent = "don't touch the code!";
+        footerText.textContent = "© 2024 All rights reserved.";
+        enButton.classList.add("active");
+        ruButton.classList.remove("active");
     }
 
     var searchInput = document.getElementById("searchInput");
